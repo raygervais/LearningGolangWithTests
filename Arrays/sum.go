1f5714e38c83884937a86a6e1a478063a650f8a7
RayGervais <raygervais@hotmail.ca>
2019-11-21 15:46:10 -0500

    adds: Array Slice examples

---
+++ Arrays/sum.go
@@ -0,0 +1,35 @@
+package main
+
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		switch len(numbers) {
+		case 0:
+			sums = append(sums, 0)
+		default:
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+
+	return sums
+}
